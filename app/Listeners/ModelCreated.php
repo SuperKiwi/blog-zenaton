12c7723446b1d1b7c7651c4206b7eba32135b49c
Martin <martingross@laposte.net>
2018-07-09 11:09:05 +0200

    Added email to admin when a new post is created

--- app/Listeners/ModelCreated.php
+++ app/Listeners/ModelCreated.php
@@ -2,9 +2,12 @@
 
 namespace App\Listeners;
 
-use App\Events\ModelCreated as EventModelCreated;
+use Notification;
+use App\Models\User;
 use App\Models\Ingoing;
 use App\Services\Thumb;
+use App\Notifications\NewPostNeedsModeration;
+use App\Events\ModelCreated as EventModelCreated;
 
 class ModelCreated
 {
@@ -18,6 +21,21 @@ class ModelCreated
     {
         $event->model->ingoing()->save(new Ingoing);
 
-        Thumb::makeThumb ($event->model);
+        Thumb::makeThumb($event->model);
+
+        if (class_basename($event->model) == 'Post') {
+            $this->postCreated($event->model);
+        }
+    }
+
+    public function postCreated($post)
+    {
+        if ($post->user->role == 'admin') {
+            return;
+        }
+
+        $adminUsers = User::whereRole('admin')->get();
+
+        Notification::send($adminUsers, new NewPostNeedsModeration($post));
     }
 }
---
+++ app/Notifications/NewPostNeedsModeration.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Post;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class NewPostNeedsModeration extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    protected $post;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line("A new post called {$this->post->title} was created")
+                    ->line('Please review it or it will be automatically validated in 2 days')
+                    ->action('Review post', url("admin/posts/{$this->post->id}"));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
